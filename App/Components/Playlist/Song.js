69f91c7ed2aa0173bb84400131f871d559123953
Jackie Wung <jacquelinewung@gmail.com>
2015-10-02 20:07:41 -0700

    [feat] song.js displays song entries

--- App/Components/Playlist/Song.js
+++ App/Components/Playlist/Song.js
@@ -1,7 +1,21 @@
 var React = require('react');
 
 var Song = React.createClass({
+  render: function() {
+    return (
+      <div className='container-playlist'>
 
+        <div className='song-view'>
+          {this.props.data.song}
+        </div>
+
+        <div className='artist-view'>
+          {this.props.data.artist}
+        </div>
+
+      </div>
+    )
+  }
 })
 
 module.exports = Song;
