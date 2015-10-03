63af2a4885376a4c804b58893fc93f608b446c22
Jackie Wung <jacquelinewung@gmail.com>
2015-10-02 20:02:17 -0700

    [feat] search.js renders searchbar

--- App/Components/Playlist/Search.js
+++ App/Components/Playlist/Search.js
@@ -1,7 +1,17 @@
 var React = require('react');
 
 var Search = React.createClass({
-
+  render: function() {
+   return (
+      <div className='searchbar-container'>
+        <div className='searchbar'>
+          <form onSubmit={this.handleSubmit}>
+            <input type='text' onSubmit={this.props.checkClick} ref='input'/>
+          </form>
+        </div>
+      </div>
+    )
+  }
 });
 
 module.exports = Search;
