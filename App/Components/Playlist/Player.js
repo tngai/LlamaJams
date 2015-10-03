38982b6f3794ce2f6dca0e9a12617bd332a9fcce
Jackie Wung <jacquelinewung@gmail.com>
2015-10-02 19:54:12 -0700

    [feat] player.js renders play and pause buttons

--- App/Components/Playlist/Player.js
+++ App/Components/Playlist/Player.js
@@ -1,7 +1,23 @@
 var React = require('react');
 
 var Player = React.createClass({
-
+  render: function() {
+    return (
+      <div className='player-container'>
+        <div className='close-container'>
+          <img src='assets/img/button-x.png' width='15' height='15'/>
+        </div>
+        <div className='play-pause'>
+          <div className='button-play'>
+            <img src='assets/img/button-play.png' width='50' height='50' ref='play' onClick={this.playShouldpause} style={displayPlay}/>
+          </div>
+          <div className='button-pause' >
+            <img src='assets/img/button-pause.png' width='50' height='50' ref='pause' onClick={this.playShouldpause} style={displayPause}/>
+          </div>
+        </div>
+      </div>
+    )
+  }
 });
 
 module.exports = Player;
